c9c351f4dca2b7d7b98cb382aa12946c1ecb7434
Roberto Araujo <roberto@localdrive.com.br>
2018-06-16 16:44:46 -0300

    remove last_name in small devices

--- resources/views/staff/msgs/index.blade.php
+++ resources/views/staff/msgs/index.blade.php
@@ -16,7 +16,9 @@
 						<img class="user-img p-2" data-bind='attr: {src: chatApp.getPicture($data.picture, "users")}'/>
 					</div>
 					<div class="col-8 col-sm-9 col-lg-10 mt-2">
-						<div data-bind="text: first_name+' '+last_name" class="chat-client-name d-block text-truncate">
+						<div class="chat-client-name d-block text-truncate">
+							<span data-bind="text: first_name"></span>
+							<span class="d-none d-md-inline-block" data-bind="text: last_name"></span>
 						</div>
 						<small class="chat-last-msg d-block text-truncate" data-bind="text: $data.last_msg ? $data.last_msg.content : ''"></small>
 					</div>
